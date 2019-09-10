8fda7e8848d682646b336429766d0494048731c9
Dmitry Dergunov <dergunovdv@yandex-team.ru>
2019-09-10 23:31:44 +0300

    change panoram photo

--- src/components/Panorama.tsx
+++ src/components/Panorama.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 // @ts-ignore
 import { Pannellum } from 'pannellum-react';
 
-import myImage from '../assets/images/pano2.jpg';
+import myImage from '../assets/images/pano.jpg';
 
 /* https://github.com/farminf/pannellum-react */
 
@@ -11,25 +11,59 @@ const Panorama: FC = () => (
     width="100vw"
     height="100vh"
     image={myImage}
-    pitch={10}
-    yaw={180}
-    hfov={110}
+    pitch={0}
+    yaw={68}
+    hfov={80}
     title="360° Panorama example"
-    author={"<span>univay. (Dmitry Dergunov)<br /><a class=\"link\" href=\"https://t.me/dergunov_dmitry\">Получить альфа-доступ</a></span>"}
+    author={
+      '<span>univay. (Dmitry Dergunov)<br /><a class="link" href="https://t.me/dergunov_dmitry">Получить альфа-доступ</a><br /></span>'
+    }
     autoLoad
     onLoad={() => {
       console.log('panorama loaded');
     }}
   >
-    <Pannellum.Hotspot type="info" pitch={-5} yaw={-60} text="TV" />
+    <Pannellum.Hotspot
+      type="info"
+      pitch={-9}
+      yaw={-72}
+      text="Большая варочная панель с духовкой"
+    />
 
-    <Pannellum.Hotspot type="info" pitch={2} yaw={-172} text="Big window" />
+    <Pannellum.Hotspot
+      type="info"
+      pitch={-11}
+      yaw={33}
+      text="Диван с набором подушек"
+    />
 
-    <Pannellum.Hotspot type="info" pitch={-12} yaw={112} text="King-Size bed" />
+    <Pannellum.Hotspot
+      type="info"
+      pitch={-2}
+      yaw={88}
+      text="Большая летняя веранда"
+    />
 
-    <Pannellum.Hotspot type="info" pitch={0} yaw={23} text="Big mirror" />
+    <Pannellum.Hotspot
+      type="info"
+      pitch={-9}
+      yaw={119}
+      text="Стеклянная лестница"
+    />
 
-    <Pannellum.Hotspot type="info" pitch={-10} yaw={26} text="Table" />
+    <Pannellum.Hotspot
+      type="info"
+      pitch={-25}
+      yaw={-59}
+      text="Обеденный стол из натурального дерева"
+    />
+
+    <Pannellum.Hotspot
+      type="info"
+      pitch={31}
+      yaw={62}
+      text="Дизайнерское освещение"
+    />
   </Pannellum>
 );
 
